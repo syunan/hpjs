029e1c10d3514b99b5bc5add77cef29c38c090b2
syunan <syunan_uchitomi@sirius.ocn.ne.jp>
2017-01-04 20:10:53 +0900

    Add files via upload

---
+++ hpmenu.js
@@ -0,0 +1,9 @@
+$(function() {
+var tag = '<img src="http://q2.upup.be/f/r/ztBkP7zCxv.png""width="50"><a href="#p1"><img src="http://q2.upup.be/f/r/JKU9nDhQxw.png"alt="当店からのお知らせ""width="50"></a><a href="#p2"><img src="http://q2.upup.be/f/r/ob0qE7hzMO.png"alt="新台入替情報""width="50"></a><a href="#p3"><img src="http://q2.upup.be/f/r/e1FObP5rD2.png"alt="設備""width="50"></a><a href="#p4"><img src="http://q2.upup.be/f/r/ZfUIQhI0Mg.png"alt="入場案内""width="50"></a><a href="#p5"><img src="http://q2.upup.be/f/r/Z65VKH41RR.png"alt="Floor Map""width="50"></a><a href="#p6"><img src="http://q2.upup.be/f/r/5oj3scOGQH.png"alt="求人案内""width="50"></a><a href="#p7"><img src="http://q2.upup.be/f/r/Tx0FlFnj0C.png"alt="設置機種""width="50"></a></div>';
+
+$('#hpmenu').html(tag);
+
+$('#hpmenu').css("display","");
+
+$('#hpmenu').exFixed();
+});
